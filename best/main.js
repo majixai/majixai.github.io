0d440d23dc46f5900a42d3e43407a9ba084fe1ee
majixai <jamhub25@gmail.com>
2025-02-19 08:07:30 -0800

    Create main.js

---
+++ best/main.js
@@ -0,0 +1,13 @@
+// main.js
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js') // Path to your service-worker.js file
+    .then(registration => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(error => {
+      console.error('Service Worker registration failed:', error);
+    });
+} else {
+  console.log('Service Workers are not supported by this browser.');
+}
