5bd6709d7fe4ea96305beae516c585bcc0bc27c0
majixai <jamhub25@gmail.com>
2025-03-24 20:58:06 -0700

    Update main.js

--- market/script/main.js
+++ market/script/main.js
@@ -1,6 +1,8 @@
 import { market } from './Market.js';
 
 $(document).ready(function(){
+    $('#fetch-data-button').click(() => market.fetchMarketData());
+    
     // Fetch data and update chart every 60 seconds
     market.fetchDataAndUpdateChart();
     setInterval(() => market.fetchDataAndUpdateChart(), 60000);
